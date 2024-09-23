00dc0c06e93e5be975c76fec7f6eed2155bcafa1
Gur Shalev <shalevgur1@gmail.com>
2024-09-24 00:24:47 +0300

    inital project with jsx and backend

--- src/dkeeper_backend/main.mo
+++ src/dkeeper_backend/main.mo
@@ -8,15 +8,27 @@ actor {
     content: Text;
   };
   
-  var notes: List.List<Note> = List.nil<Note>();
+  stable var notes: List.List<Note> = List.nil<Note>();
 
   public func createNote(titleText: Text, contentText: Text) {
     let newNote: Note = {
       title = titleText;
       content = contentText;
     };
-
     notes := List.push(newNote, notes);
-    Debug.print(debug_show(notes));
   };
+
+  // Returnning an array instead of list.
+  // The reason is because it is more easy to work with arrays in JS
+  // but more effecient to work with lists on the blockchain using Motoko
+  public query func readNotes(): async [Note] {
+    return List.toArray(notes);
+  };
+
+  public func removeNote(index: Nat) {
+    var firstPart = List.take(notes, index);
+    notes := List.drop(notes, index+1);
+    notes := List.append(firstPart, notes);
+    Debug.print(debug_show(notes));
+  }
 };
--- src/dkeeper_frontend/src/components/App.jsx
+++ src/dkeeper_frontend/src/components/App.jsx
@@ -1,20 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
+import {dkeeper_backend as dkeeper} from "../../../declarations/dkeeper_backend";
 
 function App() {
   const [notes, setNotes] = useState([]);
 
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    const notesArray = await dkeeper.readNotes();
+    console.log(notesArray);
+    setNotes(notesArray);
+  }
+
   function addNote(newNote) {
     setNotes(prevNotes => {
-      return [...prevNotes, newNote];
+      dkeeper.createNote(newNote.title, newNote.content);
+      return [newNote, ...prevNotes];
     });
   }
 
   function deleteNote(id) {
     setNotes(prevNotes => {
+      dkeeper.removeNote(id);
       return prevNotes.filter((noteItem, index) => {
         return index !== id;
       });
